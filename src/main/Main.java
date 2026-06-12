38a0039365eb7018015c675f72c718bdb783e379
Eman Firooz <emanfirooz@gmail.com>
2026-06-12 10:42:41 +0430

    check for error, first commit initialized

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String []args) {
+	
+		JFrame Window = new JFrame("Simple Chess");
+		
+		Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Window.setBackground(Color.BLACK);
+		Window.setResizable(false);
+
+	}
+}
